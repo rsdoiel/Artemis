b9945c6b2f62444a55b69fb23c034537bea5dc3b
R. S. Doiel <rsdoiel@gmail.com>
2021-09-27 10:26:22 -0700

    Added placeholders for Math and Out for the Oxford obc-3 compiler

---
+++ oxford/artMath.m
@@ -0,0 +1,4 @@
+(** artMath.m this implements the Math module based on the Oakwood Guidelines for the Oxford, obc-3, compiler *)
+MODULE artMath;
+
+END artMath.
---
+++ oxford/artOut.m
@@ -0,0 +1,4 @@
+(** artOut.m this implements the Out module based on the Oakwood Guidelines for the Oxford, obc-3, compiler *)
+MODULE artOut;
+
+END artOut.
